51fe01f5712eaa067aac34c72e0ed3c47ad230f9
joaopedsa <joaopedrosantana1997@hotmail.com.br>
2019-02-10 02:05:19 -0200

    :D

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-mongoose.connect('mongodb+srv://joaopedsa:jp19971302@cluster0-ou6vs.gcp.mongodb.net/goweek?retryWrites=true',{useNewUrlParser: true})
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0-ou6vs.gcp.mongodb.net/${process.env.DB_NAME}?retryWrites=true`,{useNewUrlParser: true})
 .then(console.log('Conectou'))
 .catch(err => console.log(err))
 
@@ -18,8 +19,8 @@ app.use((req,res,next) => {
 })
 app.use(cors())
 app.use(express.json());
-app.use(require('../src/routes'));
+app.use(require('./routes'));
 
-server.listen(3000, () => {
-    console.log('Server Started on port 3000')
+server.listen(8000 , () => {
+    console.log('Server Started on port 8000')
 });
